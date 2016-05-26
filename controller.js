465b51469ea8628c98161e0f4b625126d269826b
tsingleton5 <tjsingleton5@gmail.com>
2016-05-26 12:05:14 -0600

    done

--- controller.js
+++ controller.js
@@ -1,7 +1,14 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  $scope.currentPage = 1;
+  $scope.totalPages = 2;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	var promise = mainService.getUsers($scope.currentPage)
+                promise.then(function(response){
+                  $scope.users = response.data.data;
+                  $scope.totalPages = response.data.total_pages;
+                })
   }
 
 
--- service.js
+++ service.js
@@ -1,28 +1,12 @@
 angular.module('userProfiles').service('mainService', function($http) {
 
-	var data = [
-    {
-        "id": 1,
-        "first_name": "george",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-    },
-    {
-        "id": 2,
-        "first_name": "lucille",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-    },
-    {
-        "id": 3,
-        "first_name": "oscar",
-        "last_name": "bluth",
-        "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-    }
-  ];
+  this.getUsers = function(pageNum) {
+      var promise = $http({
+          method: 'GET',
+          url: 'http://reqres.in/api/users?page=' + pageNum
+      })
 
-  this.getUsers = function() {
-    return data;
+      return promise;
   };
 
 
